0fa6817b6ae021dcc1a369ddc4cadab06ec11910
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2021-05-21 10:36:09 +0530

    Add files via upload

---
+++ control statement (if else).py
@@ -0,0 +1,20 @@
+a=int(input())
+#check whether a is <10 or not
+if a<10:
+	  print('a is less than 10')
+	  print('Hi..............')
+else:
+	  print('a is greater than 10')
+	  print('Hello.....')	  
+	  
+	  '''
+	  output:1
+	  8
+	  a is less than 10
+	  Hi.........
+	  
+	  output:2
+	  18
+	  a is greater than 10
+	  Hello........
+	  '''
