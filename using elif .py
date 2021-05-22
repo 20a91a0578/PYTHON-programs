ffa72c3da29974bf78220c8e88e33a0b1b8d5910
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2021-05-22 15:24:32 +0530

    Add files via upload

---
+++ using elif .py
@@ -0,0 +1,32 @@
+n=int(input())
+if n%2==0:
+	print('divisble by 2')
+elif n%3==0:
+	print('divisible by 3')
+elif n%4==0:
+	print('divisible by 4')
+elif n%5==0:
+	print('divisible by 5')
+else:
+	print('Number is not divisible by 2or3or4or5')
+	
+	'''
+	output1
+	8
+	divisible by 2
+	
+	output2
+	3
+	divisible by 3
+	
+	output
+	25
+	divisible by 5
+	
+	output
+	77
+	Number is not divisble by 2or3or4or5
+	
+	'''
+	
+	
