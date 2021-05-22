1204eb35fc9ce24b4a5be4561d63e46f07d59132
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2021-05-22 15:23:41 +0530

    Add files via upload

---
+++ elif example 2.py
@@ -0,0 +1,29 @@
+n=int(input())
+if n%2!=0:
+	print('weird')
+elif 2<=n<=5:
+	print('not weird')
+elif 6<=n<=20:
+	print('weird')
+else:
+    print('not weird')	
+    
+ ''' 
+  output:1
+  4
+  not weird
+  
+  output:2
+  8
+weird
+  
+ output:3
+22
+  not weird
+  
+  output:4
+ 5
+ weird
+  
+  
+  
