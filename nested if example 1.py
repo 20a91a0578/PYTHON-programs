ebf189a44a7a6cf69d425e6532950c4c1f458864
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2021-05-28 07:39:12 +0530

    Add files via upload

---
+++ nested if example 1.py
@@ -0,0 +1,9 @@
+username=input()
+if username=="CSEB":
+	 password=input()
+	 if password=="AEC":
+	 	print('valid student')
+	 else:
+	 	print('invalid password')
+else:
+	print('invalid user name')
