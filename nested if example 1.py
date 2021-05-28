acb59e771032f52e9d99ffd31a6ea31c20912d57
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2021-05-28 07:59:33 +0530

    Update nested if example 1.py

--- nested if example 1.py
+++ nested if example 1.py
@@ -7,3 +7,19 @@ if username=="CSEB":
 	 	print('invalid password')
 else:
 	print('invalid user name')
+
+'''
+Output:1
+CSEB
+AEC
+valid student
+
+output:2
+CSEB
+ABC
+invalid password
+
+output:3
+CSEC
+invalid username
+'''
