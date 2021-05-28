0fa931f1c9da1e6d5ef36a6607695aa20a818fc1
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2021-05-28 08:24:09 +0530

    Update while loop ex2.py

--- while loop ex2.py
+++ while loop ex2.py
@@ -4,3 +4,19 @@ while i<=10:
 	i+=2
 print('After while loop the value of i is{}'.format(i))
 print('After while loop the value of i is %d'%(i))
+
+'''
+Output
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+After while loop the value of is 11
+After while loop value of i is 11
+'''
