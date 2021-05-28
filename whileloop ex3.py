88368c279a16354d1d57a9d89a37f6f26d9bd717
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2021-05-28 07:52:26 +0530

    Add files via upload

---
+++ whileloop ex3.py
@@ -0,0 +1,7 @@
+start_value=int(input('Enter starting value'))
+end_value=int(input('Enter ending value'))
+stepcount=int(input('Enter stepcount'))
+while start_value<=end_value:
+	print(start_value)
+	start_value+=stepcount
+
