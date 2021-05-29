af5aed4c97cb1f78c2979f4d62f3a26e72d78d57
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2021-05-29 15:50:34 +0530

    Add files via upload

---
+++ finding last digit of A^B.py
@@ -0,0 +1,15 @@
+A=int(input('Enter A:'))
+B=int(input("Enter B:"))
+print('last digit of A^B is:',(A**B)%10)
+'''
+output:1
+Enter A:6
+Enter B:9
+last digit of A^B is:6
+
+output:2
+Enter A:99
+Enter B:76
+last digit of A^B is:1
+
+'''
