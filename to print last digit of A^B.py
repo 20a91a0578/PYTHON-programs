38bdd57600dea15db9a364765035a1bec068086c
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2021-05-31 09:53:40 +0530

    Update to print last digit of A^B.py

--- to print last digit of A^B.py
+++ to print last digit of A^B.py
@@ -1,3 +1,15 @@
 A=int(input('Enter A:'))
 B=int(input("Enter B:"))
 print('last digit of %d^%d is :'%(A,B),(A**B)%10)
+
+'''
+Output:1
+Enter A:8
+Enter B:3
+last digit of 8^3 is: 2
+
+Output:2
+Enter A:9
+Enter B:7
+last digit of 9^7 is :9
+'''
